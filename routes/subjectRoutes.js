5dd8c03db12776b2ecf289e4b34816d964381aad
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2025-01-07 12:28:08 +0500

    update subject api

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -23,6 +23,25 @@ router.post('/create', async (req, res) => {
 	}
 })
 
+router.patch('/:id', async (req, res) => {
+	try {
+		const {id} = req.params
+		const {name} = req.body
+		
+		const existSubject = await Subject.findOne({_id: id})
+		if (!existSubject) {
+			res.status(404).json({success: false, message: "Subject not found!"})
+		}
+		
+		existSubject.name = name
+		const result = await existSubject.save()
+		res.status(200).json({success: true, data: result, message: "Updated"})
+	} catch (e) {
+		console.log(e)
+		res.status(500).json({error: e.message})
+	}
+})
+
 router.delete('/:id', async (req, res) => {
 	try {
 		const {id} = req.params
